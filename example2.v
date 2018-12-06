bea4183c572bba948bb8a4a77e60c7b12d418cd5
0918nobita <nobita.0918@gmail.com>
2018-12-06 20:35:54 +0900

    :sparkles: 局所的な名前束縛のサンプルを追加した

--- example2.v
+++ example2.v
@@ -7,3 +7,17 @@ Definition double x := x + x.
 Print double. (* double = fun x : nat => x + x : nat -> nat *)
 
 Eval compute in double 2. (* = 4 : nat (式の計算) *)
+
+(* 局所的な名前束縛 *)
+Definition quad x := let y := double x in 2 * y.
+Eval compute in quad 2. (* = 8 : nat *)
+
+Definition quad' x := double (double x).
+Eval compute in quad' 2.
+
+Definition triple x :=
+  let double x := x + x in
+  double x + x.
+
+Eval compute in triple 3. (* = 9 : nat *)
+Eval compute in 1 - 2. (* = 0 : nat (自然数の引き算は整数と違う) *)
