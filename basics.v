0bcf0f3299655ce460fd4a72d3aba94c257bbaa1
0918nobita <nobita.0918@gmail.com>
2018-12-09 02:47:43 +0900

    :sparkles: orb 関数の単体テストを追加した

--- basics.v
+++ basics.v
@@ -57,3 +57,15 @@ Definition orb (b1 b2 : bool) : bool :=
   | true => true
   | false => b2
   end.
+
+Example test_orb1 : (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb2 : (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb3 : (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb4 : (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
