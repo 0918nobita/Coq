b68f8f843f2bb8bb82f0d860674457595a8b0f37
0918nobita <nobita.0918@gmail.com>
2018-12-09 02:36:22 +0900

    :sparkles: 列挙型の値を引数に取る関数を定義するサンプルを追加した

--- basics.v
+++ basics.v
@@ -7,3 +7,15 @@ Inductive day: Type :=
   | friday : day
   | saturday : day
   | sunday : day.
+
+(* 関数定義 *)
+Definition next_weekday (d : day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
