5e94c0a1eff87f6e74922a83f35ebe543f8b8b99
0918nobita <nobita.0918@gmail.com>
2018-12-09 02:35:16 +0900

    :sparkles: Inductive 構文を用いて列挙型を定義するサンプルを追加した

---
+++ basics.v
@@ -0,0 +1,9 @@
+(* 新しいデータ型のセット (集合) である「列挙型」 *)
+Inductive day: Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
