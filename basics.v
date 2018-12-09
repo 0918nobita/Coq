2a72a014dc61e63b05433401581bbc39ad6052b0
0918nobita <nobita.0918@gmail.com>
2018-12-09 16:25:16 +0900

    :white_check_mark: oddb 関数の単体テストを追加した

--- basics.v
+++ basics.v
@@ -141,3 +141,9 @@ Fixpoint evenb (n : nat) : bool :=
   end.
 
 Definition oddb (n : nat) : bool := negb (evenb n).
+
+Example test_oddb1 : (oddb (S O)) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_oddb2 : (oddb (S(S(S(S( O)))))) = false.
+Proof. simpl. reflexivity. Qed.
