811b7e0ec17154cb4d7f99abe1cf32433452cd98
0918nobita <nobita.0918@gmail.com>
2018-12-09 14:29:26 +0900

    :white_check_mark: andb3 関数の単体テストを追加した

--- basics.v
+++ basics.v
@@ -87,3 +87,15 @@ Example test_nandb4 : (nandb true true) = false.
 Proof. simpl. reflexivity. Qed.
 
 Definition andb3 (b1 b2 b3 : bool) : bool := andb (andb b1 b2) b3.
+
+Example test_andb31 : (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb32 : (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb33 : (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb34 : (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
