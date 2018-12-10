8ad556b1b154b8e7e911d1618a6fcc0341f122b4
0918nobita <nobita.0918@gmail.com>
2018-12-10 20:46:47 +0900

    :sparkles: 定理「forall n m : nat, (0 + n) * m = n * m」を証明した

--- basics.v
+++ basics.v
@@ -275,3 +275,11 @@ Proof.
   rewrite H2.
   reflexivity.
 Qed.
+
+Theorem mult_0_plus : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  rewrite -> plus_0_n.
+  reflexivity.
+Qed.
