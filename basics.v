403fe9fcaf30f9c4076bc7f886e09a7442059adc
0918nobita <nobita.0918@gmail.com>
2018-12-10 20:49:00 +0900

    :sparkles: 定理「forall n : nat, beq_nat (n + 1) 0 = false」を証明した
    destruct タクティックを用いている

--- basics.v
+++ basics.v
@@ -292,3 +292,12 @@ Proof.
   simpl.
   reflexivity.
 Qed.
+
+Theorem plus_1_neq_0_firsttry : forall n : nat,
+  beq_nat (n + 1) 0 = false.
+Proof.
+  intros n.
+  destruct n as [|n'].
+    reflexivity.
+    reflexivity.
+Qed.
