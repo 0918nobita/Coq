06e4fdbcc52483dc94f24f90188d9bdeb0c431f2
0918nobita <nobita.0918@gmail.com>
2018-12-10 20:47:30 +0900

    :sparkles: 定理「forall n m : nat, (1 + n) * m = m + (n * m)」を証明した

--- basics.v
+++ basics.v
@@ -283,3 +283,12 @@ Proof.
   rewrite -> plus_0_n.
   reflexivity.
 Qed.
+
+Theorem mult_1_plus : forall n m : nat,
+  (1 + n) * m = m + (n * m).
+Proof.
+  intros n m.
+  rewrite -> plus_1_l.
+  simpl.
+  reflexivity.
+Qed.
