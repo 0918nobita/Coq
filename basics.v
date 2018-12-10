7437e7f5e402879a992cda129800ee06d0877fea
0918nobita <nobita.0918@gmail.com>
2018-12-10 21:27:50 +0900

    :sparkles: 定理「forall b : bool, negb (negb b) = b」を証明した

--- basics.v
+++ basics.v
@@ -302,6 +302,14 @@ Proof.
     reflexivity.
 Qed.
 
+Theorem negb_involutive : forall b : bool, negb (negb b) = b.
+Proof.
+  intros b.
+  destruct b.
+    reflexivity.
+    reflexivity.
+Qed.
+
 Theorem zero_nbeq_plus_1 : forall n : nat,
   beq_nat 0 (n + 1) = false.
 Proof.
